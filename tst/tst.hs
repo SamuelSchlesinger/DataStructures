c81f4e26fa6079f36d15ea3c98492629c7b5fb36
Samuel Schlesinger <sgschlesinger@gmail.com>
2016-05-11 10:21:33 -0400

    Added generic Binary instance

--- tst/tst.hs
+++ tst/tst.hs
@@ -1,12 +1,18 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+import Data.Binary
+import GHC.Generics (Generic)
 
 data T c v where
-    N :: (Ord c) => T c v -> T c v -> (c, Maybe v) -> T c v -> T c v
-    E :: T c v
+    N :: T c v -> T c v -> (c, Maybe v) -> T c v -> T c v
+    E :: T c v deriving (Generic)
+
+instance (Binary c, Binary v) => Binary (T c v)
 
 insert :: (Ord c) => T c v -> ([c], v) -> T c v
 
-get :: (Ord c) => T c v -> [c] -> Maybe v
+snag :: (Ord c) => T c v -> [c] -> Maybe v
 
 -- | This took me a weirdly long amount of time to reason about.
 -- | Ended up being far shorter and more coherent than the Java version.
@@ -27,24 +33,24 @@ insert (N l d (c', v') r) (s@(c:cs), v)
   | c == c' = (N l (insert d (cs, v)) (c', v') r) -- go down
   | c >  c' = (N l d (c', v') (insert r (s, v))) -- go right
 
-get _ [] = Nothing -- wat is nun?
+snag _ [] = Nothing -- wat is nun?
 
-get E _  = Nothing -- heh?
+snag E _  = Nothing -- heh?
 
-get (N l d (c', v') r) s@(c:[])
-  | c <  c' = get l s -- go left
+snag (N l d (c', v') r) s@(c:[])
+  | c <  c' = snag l s -- go left
   | c == c' = v' -- return dat thang
-  | c >  c' = get r s -- go right
+  | c >  c' = snag r s -- go right
 
-get (N l d (c', v') r) s@(c:cs)
-  | c <  c' = get l s -- g'left
-  | c == c' = get d cs -- g'down
-  | c >  c' = get r s --g'right
+snag (N l d (c', v') r) s@(c:cs)
+  | c <  c' = snag l s -- g'left
+  | c == c' = snag d cs -- g'down
+  | c >  c' = snag r s --g'right
 
 test_input = [(x ++ y ++ z, x ++ y ++ z) | x <- ["hi", "ho", "he", "ca", "cu", "ke", "pu", "pi"], y <- ["bi", "bo", "bu", "ba", "be", "la", "ar"], z <- ["re", "ro", "ra", "rq", "rk"]]
 
 test_tst = foldl insert E test_input
 
 test = let input = map fst test_input 
-           in let results = map (get test_tst) input
+           in let results = map (snag test_tst) input
                   in (map (\(Just x) -> x)  results) == input
